72c912dc258b9bd6049792538d31f17d2b442f63
Ian Jones <ianj1993@gmail.com>
2024-01-06 12:18:30 -0700

    Added logic for calculator num buttons, clear button and delete button

---
+++ script.js
@@ -0,0 +1,55 @@
+const clearButton = document.getElementById('clearButton');
+const deleteButton = document.getElementById('deleteButton');
+const lastCalc = document.getElementById('lastCalc');
+const currentCalc = document.getElementById('currentCalc');
+const buttonArray = document.querySelectorAll('[data-number]');
+const operatorArray = document.querySelectorAll('[data-operator]');
+
+clearButton.addEventListener('click', () => {
+    clear();
+})
+
+deleteButton.addEventListener('click', () => {
+    if (currentCalc.textContent == '') {
+        currentCalc.textContent = '0';
+    }
+    else {
+        currentCalc.textContent = currentCalc.textContent.slice(0, -1);
+    }
+    
+})
+
+buttonArray.forEach(button => {
+    button.addEventListener('click', function() {
+        if (currentCalc.textContent == '0') {
+            currentCalc.textContent = '';
+            currentCalc.textContent += button.textContent;
+        }
+        else {
+            currentCalc.textContent += button.textContent;
+        }
+        
+    });
+});
+
+function add(input1, input2) {
+    return input1 + input2;
+}
+
+function divide(input1, input2) {
+    return input1 / input2;
+}
+
+function multiply(input1, input2) {
+    return input1 * input2;
+}
+
+function subtract(input1, input2) {
+    return input1 - input2;
+}
+
+
+function clear() {
+    currentCalc.textContent = '0';
+    lastCalc.textContent = '';
+}
